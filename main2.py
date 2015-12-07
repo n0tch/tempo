e36e1ae6f59ecb205fd2b75681e1ec2d12e95ac9
n0tch <gusoliv3ira@gmail.com>
2015-12-07 00:24:49 -0200

    Aplicativo para consultas meteorologicas

--- main2.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-
-dado = requests.get("http://developers.agenciaideias.com.br/tempo/json/sao paulo - SP")
-dado = json.loads(dado.text)
-
-for num, key in enumerate(dado['previsoes']):
-	if num <= 3:
-		print(num, key['data'])
